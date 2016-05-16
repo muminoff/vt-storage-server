70545bb8b0130104d19ac692eae8c7686daad882
muminoff <smuminov@gmail.com>
2016-05-16 22:29:16 +0900

    Updated config

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-AWS_APP_ID = "AKIAIPKFYIRA2QY3IKVA"
-AWS_APP_SECRET = "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"
-AWS_REGION = "ap-northeast-2"
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+AWS_APP_ID = "AWS_APP_ID"
+AWS_APP_SECRET = "ФАК_Ю :)"
+AWS_REGION = "ap-northeast-1"
